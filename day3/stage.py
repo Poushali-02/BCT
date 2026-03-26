8780df9593c1258eaf25f5b54664111e9fa57339
Poushali Bhattacharyya <poushali.bhattacharyya@stepoutplay.com>
2026-03-26 10:18:01 +0530

    day 3 class

---
+++ day3/stage.py
@@ -0,0 +1,13 @@
+age = int(input("Enter age : "))
+
+if age >= 0 and age <= 5:
+    print("You're still a kid")
+    
+elif age >= 6 and age <= 11:
+    print("You're a child you don't know anything")
+    
+elif age >= 12 and age <= 19:
+    print("You're a teen")
+    
+else:
+    print("Buro")
---
+++ day3/swap.py
@@ -0,0 +1,7 @@
+a = int(input("Enter data a "))# 12
+b = int(input("Enter data b "))# 13
+
+a = a + b # 25
+b = a - b # 12
+a = a - b # 13
+print(f"After swapping a={a}, b={b}")
